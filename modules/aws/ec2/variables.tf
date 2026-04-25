78a507699d0a7c2ac7737117def3fc0523583a57
pavaniam214 <55245747+pavaniam214@users.noreply.github.com>
2026-04-25 12:45:44 +0530

    Refactor variable definitions in variables.tf

--- modules/aws/ec2/variables.tf
+++ modules/aws/ec2/variables.tf
@@ -1,24 +1,31 @@
-variable "region"
-{
-    description=""
-    type=string
+variable "region" {
+  description = "AWS region"
+  type        = string
+#  default     = "us-east-1"
 }
 
-variable "ami_id"{
-    description="AMI_ID"
-    type=string
-    default="ami-098e39bafa7e7303d"
+variable "ami_id" {
+  description = "AMI_ID"
+  type        = string
+  default     = "ami-098e39bafa7e7303d"
 }
 
-variable "instance_type"
-{
-    description ="Ec2 instance type"
-    type=string
-    default="t3.micro"
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
 }
 
-variable "key_name"{
-    description ="Ec2 key pair name"
-    type=string
-    default="pair1"
+variable "key_name" {
+  description = "EC2 key pair name"
+  type        = string
+  default     = "pair1"
+}
+
+
+
+variable "availability_zone" {
+  description = "Availability Zone for resources"
+  type        = string
+  default     = "us-east-1b"
 }
